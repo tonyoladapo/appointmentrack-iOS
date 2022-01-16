283158de1808f9dede194f9b7f0dab3ae5dce6f8
Tony <toneyobaze@gmail.com>
2022-01-16 17:52:13 +0000

    create apponmtments flatlist

--- src/screens/bottomTabs/Home.tsx
+++ src/screens/bottomTabs/Home.tsx
@@ -1,14 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import useAppointments from '../../hooks/useAppointments';
+import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import { RootNavigationProp } from '../../types/navigation';
 
 const Home = () => {
+  const { getAppointments, appointments, loading, addAppointment } =
+    useAppointments();
+
+  useEffect(() => {
+    getAppointments();
+  }, []);
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => auth().signOut()}>
+        <Text>Sign out</Text>
+      </TouchableOpacity>
+
+      <FlatList
+        data={appointments}
+        keyExtractor={({ id }, index) => `${index}-${id}`}
+        renderItem={({ item }) => <Item item={item} />}
+      />
     </View>
   );
 };
 
-export default Home;
+const Item = ({ item }: any) => {
+  const { navigate } = useNavigation<RootNavigationProp>();
 
-const styles = StyleSheet.create({});
+  return (
+    <TouchableOpacity
+      onPress={() => navigate('AppointmentDetails', { item })}
+      style={{ padding: 16, backgroundColor: 'tomato', marginVertical: 2 }}>
+      <Text>{item.name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Home;
