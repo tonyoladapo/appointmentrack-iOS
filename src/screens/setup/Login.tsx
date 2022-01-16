7c23ab0031f142357b86d29bfd548803b4b84fa0
Tony <toneyobaze@gmail.com>
2022-01-16 13:09:21 +0000

    create Sign in and skip buttons

--- src/screens/setup/Login.tsx
+++ src/screens/setup/Login.tsx
@@ -1,14 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+} from 'react-native';
+import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import useAuth from '../../hooks/useAuth';
 
 const Login = () => {
+  const { signInWithGoogle, signInAnonymously, authenticating } = useAuth();
+
   return (
-    <View>
-      <Text></Text>
+    <View style={[styles.container]}>
+      <View
+        style={[
+          StyleSheet.absoluteFill,
+          {
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        ]}>
+        <GoogleSigninButton
+          style={{ width: 192, height: 48, borderRadius: 10 }}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Light}
+          onPress={signInWithGoogle}
+          disabled={authenticating}
+        />
+
+        <TouchableOpacity
+          onPress={signInAnonymously}
+          style={{ padding: 16 }}
+          disabled={authenticating}>
+          <Text>Skip</Text>
+        </TouchableOpacity>
+      </View>
+
+      {authenticating && (
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <ActivityIndicator animating={authenticating} />
+        </View>
+      )}
     </View>
   );
 };
 
-export default Login;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default Login;
