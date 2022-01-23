ac6548f67b67a156c82b77712d8d777f25bd4659
Tony <toneyobaze@gmail.com>
2022-01-23 07:02:09 +0000

    update appointment type

--- src/types/appointment.ts
+++ src/types/appointment.ts
@@ -1,19 +1,20 @@
 export interface AppointmentTypes {
   id: string;
   title: string;
-  start: Date;
-  end?: Date;
+  date: Date;
+  startTime: Date;
+  endTime?: Date;
   allDay?: boolean;
-  location?: string;
+  // location?: string;
   notes?: string;
-  type?: string;
-  person: PersonTypes;
+  // type?: string;
+  person: PersonTypes | null;
 }
 
 export interface PersonTypes {
   id: string;
   firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
 }
