e728fdb86558752aa891c111e50c2ed4b024f3cd
Tony <toneyobaze@gmail.com>
2022-01-23 06:59:41 +0000

    create setPickedPerson action

--- src/actions/appointment.ts
+++ src/actions/appointment.ts
@@ -1,8 +1,16 @@
+import { PersonTypes } from './../types/appointment';
 import { AppointmentTypes } from '../types/appointment';
 
 export const setAppointments = (appointments: AppointmentTypes[]) => {
   return {
     type: 'SET_APPOINTMENTS',
     payload: appointments,
+  };
+};
+
+export const setPickedPerson = (pickedPerson: PersonTypes | null) => {
+  return {
+    type: 'SET_PICKED_PERSON',
+    payload: pickedPerson,
   };
 };
