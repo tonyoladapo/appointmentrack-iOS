5e3753b551db2250b33aca293537c1c34c1a5f04
Tony <toneyobaze@gmail.com>
2022-01-31 00:08:25 +0000

    create personpicker flatlist

--- src/components/modals/PersonPickerModal.tsx
+++ src/components/modals/PersonPickerModal.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { StyleSheet, useWindowDimensions } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import { Modalize } from 'react-native-modalize';
+import { ReducerTypes } from '../../types/main';
+import { PersonTypes } from '../../types/appointment';
+import { setPickedPerson } from '../../actions/appointment';
 
 interface Props {
   modalRef: React.RefObject<Modalize>;
@@ -10,9 +19,50 @@ const PersonPickerModal = ({ modalRef }: Props) => {
   const { height } = useWindowDimensions();
   const modalHeight = height * 0.7;
 
-  return <Modalize ref={modalRef} modalHeight={modalHeight}></Modalize>;
+  const { people } = useSelector(({ people }: ReducerTypes) => people);
+
+  return (
+    <Modalize
+      ref={modalRef}
+      modalHeight={modalHeight}
+      flatListProps={{
+        data: people,
+        keyExtractor: ({ id }, index) => `${index}-${id}`,
+        renderItem: ({ item }) => <Item person={item} modalRef={modalRef} />,
+      }}
+    />
+  );
 };
 
-const styles = StyleSheet.create({});
+const Item = ({
+  person,
+  modalRef,
+}: {
+  person: PersonTypes;
+  modalRef: React.MutableRefObject<Modalize | null>;
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => {
+        dispatch(setPickedPerson(person));
+        modalRef.current?.close();
+      }}>
+      <Text>{person.firstName}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    backgroundColor: 'tomato',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 2,
+  },
+});
 
 export default PersonPickerModal;
